32776b5bedb403ea93c0f657aaf0926527f68bcf
FrancescoSisti <francescosisti61@gmail.com>
2024-07-31 16:12:10 +0200

    added main calcule

--- script/script.js
+++ script/script.js
@@ -15,3 +15,28 @@ const age = parseInt(prompt("Inserisci la tua età"));
 console.log(`Età richiesta`, age);
 
 //Fase di elaborazione
+if (isNaN(km) || isNaN(age)) {
+    alert('Non hai inserito valori accettabili')
+} else {
+    //Calcolo del prezzo base del biglietto per km
+    const basePrice = km * 0.21;
+    console.log('Prezzo biglietto base', basePrice)
+
+    //Calcolo percentuale di sconto se minorenne oppure over 65
+    let finalPrice = basePrice;
+    //se minorenne
+    if (age < 18) {
+        finalPrice = 0.8;
+        //se over 65
+    } else if (age >= 65) {
+        finalPrice = 0.6;
+    }
+    //aggiungo due decimali al prezzo del biglietto, e il valore euro
+    finalPrice = finalPrice.toFixed(2) + '€';
+    console.log('Ticket Price', finalPrice)
+
+
+    //? FASE DI OUTPUT
+    //alert con il prezzo del biglietto
+    alert(`Grazie per averci scelto! Il prezzo del tuo biglietto è: ${finalPrice}`)
+}
